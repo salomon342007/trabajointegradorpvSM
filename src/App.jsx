b40e366ad49b25e16c9e1f1bcd52a084dd65b9fc
Modena Stefano <modenastefano00@gmail.com>
2025-06-20 18:17:36 -0300

    app realizado por StefanoModena

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { ProductProvider } from '/src/ProductContext';
-import Home from '/src/Home';
-import Favorites from '/src/Favorites';
-import ProductDetail from '/src/ProductDetail';
-import ProductForm from '/src/ProductForm';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ProductosProvider } from './context/ProductosContext';
+import AppRoutes from './AppRoutes';
+import './App.css'; // estilos de navegación, main, etc.
 
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/favorites">Favoritos</Link> | 
-        <Link to="/create">Nuevo Producto</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/create" element={<ProductForm />} />
-        <Route path="/edit/:id" element={<ProductForm />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <ProductosProvider>
+        <Router>
+          <AppRoutes />
+        </Router>
+      </ProductosProvider>
+    </AuthProvider>
   );
-};
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ProductProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ProductProvider>
-  </React.StrictMode>
-);
+export default App;

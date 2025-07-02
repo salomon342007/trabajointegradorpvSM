de15f6013a5d6fcc8d88169d661b81c538877408
FranciscoDiaz-01 <d3st31@gmail.com>
2025-07-02 15:51:25 -0300

    Diaz Francisco, comienzo login.jsx

---
+++ src/Login.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, useState } from 'react';
+import { AuthContext } from './context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password);
+    navigate('/'); // al login redirige a Inicio
+  };

5c520b6c069db5c52dca9a1f9ccf70349a5c149d
Thiago Rueda <thiagorueda2016@gmail.com>
2025-07-02 15:26:45 -0300

    favoritos:Crea el componente Favoritos con estructura básica -ThiagoRueda

---
+++ src/Favoritos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Favoritos = () => {
+  return (
+    <div>
+      <h2>Mis Favoritos</h2>
+      <p>No has marcado ningún producto como favorito.</p>
+    </div>
+  );
+};
+
+export default Favoritos;
